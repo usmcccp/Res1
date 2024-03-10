ad3863ab67e8febebb8456bd98820c75d5528d37
Yuri <145800665+usmcccp@users.noreply.github.com>
2024-03-11 01:44:27 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class rightt 
+{
+    float a_m;
+    float b_m;
+public:
+    rightt(float a, float b)
+    {
+        cout << "C" << endl;
+        a_m = a;
+        b_m = b;
+    }
+};
+
+void set(float a, float b)
+{
+    if (a < 0)
+    {
+        cout << "Negative lenth" << endl;
+        return;
+    }
+    a_m = a;
+
+    if (b < 0)
+    {
+        cout << "Negative width" << endl;
+        return;
+    }
+    b_m = b;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    rightt f(2, 3);
+    f.set(2, 3);
+
+}
+
